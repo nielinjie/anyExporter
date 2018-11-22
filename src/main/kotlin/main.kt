df05c45e2c11b67f61a48912dc3dec36f439efa2
nielinjie <nielinjie@gmail.com>
2018-11-22 18:38:57 +0800

    no message

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,20 +12,20 @@ import io.prometheus.client.CollectorRegistry
 
 
 fun main(args: Array<String>) {
-    //TODO 定时拉取
-    Timer().schedule(1000, 10000) {
-        MainPage().run()
-    }
-    //TODO push to pushGateway here
-    Timer().schedule(10000, 30000) {
-        Context.getBuildingUnits().forEach { buildingUnit ->
-            println(buildingUnit.name)
-            buildingUnit.buildings.forEach {
-                println("         " + it.number)
-            }
-        }
-    }
-
+//    //TODO 定时拉取
+//    Timer().schedule(1000, 10000) {
+//        MainPage().run()
+//    }
+//    //TODO push to pushGateway here
+//    Timer().schedule(10000, 30000) {
+//        Context.getBuildingUnits().forEach { buildingUnit ->
+//            println(buildingUnit.name)
+//            buildingUnit.buildings.forEach {
+//                println("         " + it.number)
+//            }
+//        }
+//    }
+    push()
 
 }
 fun push (){
@@ -35,15 +35,16 @@ fun push (){
     val durationTimer = duration.startTimer()
     try {
         // Your code here.
-
+        Thread.sleep(1000)
         // This is only added to the registry after success,
         // so that a previous success in the Pushgateway isn't overwritten on failure.
         val lastSuccess = Gauge.build()
             .name("my_batch_job_last_success").help("Last time my batch job succeeded, in unixtime.").register(registry)
         lastSuccess.setToCurrentTime()
+        lastSuccess.
     } finally {
         durationTimer.setDuration()
-        val pg = PushGateway("127.0.0.1:9091")
+        val pg = PushGateway("pushgateway.juma.com:80")
         pg.pushAdd(registry, "my_batch_job")
     }
 }
